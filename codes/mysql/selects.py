3d1ef8187cd30877aa268a2820ef450e1b8140ac
ARAJO <dddd2511u@gmail.com>
2023-08-04 17:59:20 +0900

    select

---
+++ codes/mysql/selects.py
@@ -0,0 +1,15 @@
+# mysql connect package
+# !pip --> conda
+
+# connet Mysql
+# ip, port, username, password, database name 
+
+# editor - statement 
+
+# make select
+
+# execute select query
+
+# display
+
+# close
